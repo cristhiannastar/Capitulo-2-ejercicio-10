85ea32a0f9d0c129889178624a0adb5159cf7119
Hp <Hp@DESKTOP-NVV8KB0>
2022-11-13 21:19:14 -0500

    Agregar archivos de proyecto.

---
+++ Capitulo 2 ejercicio 10/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ejercicio10
+{
+
+    public class program
+    {
+
+        public static void Main()
+        {
+            double pulg = 0;
+            double MT = 0.0254;
+            double Cpulg = 0;
+            double MeDados = 0;
+
+            Console.WriteLine("digite la cantidad de metros necesarios de la tela:");
+            MeDados = double.Parse(Console.ReadLine());
+            Cpulg = MeDados / MT;
+
+            Console.WriteLine($"la cantidad de pulgadas de la tela que debe pedir es :{pulg}");
+
+
+
+
+        }
+    }
+}
